04077a68255a142829679d5c29ac22946113136b
Felipe_Silvieri <felipesilvieri@yahoo.com>
2023-06-06 22:11:44 -0300

    att main

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ adc2 = ADC(Pin(27, Pin.IN))
 adc3 = ADC(Pin(28, Pin.IN))
 
 button = Pin(13, Pin.IN, Pin.PULL_DOWN)
-button2 = Pin(15, Pin.IN, Pin.PULL_DOWN)
-button3 = Pin(14, Pin.IN, Pin.PULL_DOWN)
-
+button2 = Pin(15 ,Pin.IN, Pin.PULL_DOWN)
+button3 = Pin(14 ,Pin.IN, Pin.PULL_DOWN)
 
 rele = Pin(12, Pin.OUT)
 rele2 = Pin(11, Pin.OUT)
-rele3 = Pin(10, Pin.OUT)
+rele3 = Pin(10,Pin.OUT)
 
 
 def rele_on():
@@ -47,79 +46,71 @@ def conversion_factor(value):
 def activate_bomb():
     rele_on()
 
-
 def activate_bomb2():
     rele2_on()
-
-
+    
 def activate_bomb3():
     rele3_on()
 
-
 def read_humidity():
     moisture = adc.read_u16()
-    # aumentar para prever que falte bebida
-    return conversion_factor(moisture) > 45
-
+    return conversion_factor(moisture) > 62   #aumentar para prever que falte bebida
 
 def read_humidity2():
     moisture2 = adc2.read_u16()
-    return conversion_factor(moisture2) > 45
-
+    return conversion_factor(moisture2) > 62
 
 def read_humidity3():
     moisture3 = adc3.read_u16()
-    return conversion_factor(moisture3) > 45
+    return conversion_factor(moisture3) > 62
+
 
 
 def deactivate_bomb():
     start_time = ticks_ms()
     actual_time = ticks_ms()
     moisture = (adc.read_u16())
-
-    while ((conversion_factor(moisture) >= 45) and (ticks_diff(actual_time, start_time) < 3000)):
+    
+    while ((conversion_factor(moisture) >= 45) and (ticks_diff(actual_time, start_time) < 5000)):
         moisture = (adc.read_u16())
         actual_time = ticks_ms()
 
     rele_off()
-
-    if ticks_diff(actual_time, start_time) < 3000:
+    
+    if ticks_diff(actual_time, start_time) < 5000:
         return False
-
+    
     return True
-
-
+    
 def deactivate_bomb2():
     start_time = ticks_ms()
     actual_time = ticks_ms()
     moisture = (adc2.read_u16())
-    while ((conversion_factor(moisture) >= 45) and (ticks_diff(actual_time, start_time) < 3000)):
+    while ((conversion_factor(moisture) >= 45) and (ticks_diff(actual_time, start_time) < 5000)):
         moisture = (adc2.read_u16())
         actual_time = ticks_ms()
 
     rele2_off()
-
-    if ticks_diff(actual_time, start_time) < 3000:
+    
+    if ticks_diff(actual_time, start_time) < 5000:
         return False
-
+    
     return True
-
-
+    
 def deactivate_bomb3():
     start_time = ticks_ms()
     actual_time = ticks_ms()
     moisture = (adc3.read_u16())
-    while ((conversion_factor(moisture) >= 45) and (ticks_diff(actual_time, start_time) < 3000)):
+    while ((conversion_factor(moisture) >= 45) and (ticks_diff(actual_time, start_time) < 5000)):
         moisture = (adc3.read_u16())
         actual_time = ticks_ms()
 
     rele3_off()
-
-    if ticks_diff(actual_time, start_time) < 3000:
+    
+    if ticks_diff(actual_time, start_time) < 5000:
         return False
-
+    
     return True
-
 
 # COMEÇAR INATIVO SEMPRE
 rele_off()
@@ -127,6 +118,16 @@ rele2_off()
 rele3_off()
 
 while True:
+    
+#     print(conversion_factor(adc.read_u16()))
+    
+    if (conversion_factor(adc3.read_u16())) < 62:
+        print('sem liquido na bomba 3')
+    if (conversion_factor(adc2.read_u16())) < 62:
+        print('sem liquido na bomba 2')
+    if (conversion_factor(adc.read_u16())) < 62:
+        print('sem liquido na bomba 1')    
+    
     # Combinação Bebida 1 e 2
     if button.value():
         if read_humidity2():
@@ -136,7 +137,7 @@ while True:
                 sleep(0.5)
                 activate_bomb2()
                 deactivate_bomb2()
-
+    
     # Combinação Bebida 2 e 3
     if button2.value():
         if read_humidity3():
@@ -146,7 +147,7 @@ while True:
                 sleep(0.5)
                 activate_bomb3()
                 deactivate_bomb3()
-
+        
     # Combinação Bebida 3 e 1
     if button3.value():
         if read_humidity():
@@ -156,5 +157,5 @@ while True:
                 sleep(0.5)
                 activate_bomb()
                 deactivate_bomb()
-
+   
     sleep(0.1)
